b8503f10d9aeea600c38ad5dbdfe9435458276f1
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-10-03 15:56:47 +0900

    外部リンクにアイコンを追加

--- .vitepress/theme/components/VPLink.vue
+++ .vitepress/theme/components/VPLink.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-import { computed, onBeforeMount, onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { isActiveLink } from '../composable/isActiveLink';
 import { guessLinkTarget } from '../util/URL';
+import { GSymbol } from 'vue-material-symbols';
 
 const props = defineProps<{
   href: string
@@ -20,9 +21,27 @@ onMounted(() => {
 </script>
 
 <template>
-  <a :href="props.href" :target="target" :class="{ active: imActive }"><slot></slot></a>
+  <a :href="props.href" :target="target" :class="{ active: imActive }">
+    <span class="inline">
+      <span class="text">
+        <slot></slot>
+      </span>
+      <GSymbol v-if="target === '_blank'" icon="open_in_new" :size="20"/>
+    </span>
+  </a>
 </template>
 
 <style lang="scss" scoped>
+a {
+  
+  .inline {
+    display: inline-flex;
+    width: 100%;
 
+    .g-symbol {
+      vertical-align: middle;
+      margin-left: auto;
+    }
+  }
+}
 </style>
